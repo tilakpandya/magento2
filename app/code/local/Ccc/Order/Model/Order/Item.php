93c0b98b9911fd61e70abb62e542f96239d279e1
unknown <tilakpandya2@gmail.com>
2021-05-24 09:50:23 +0530

    24/5/2021

---
+++ app/code/local/Ccc/Order/Model/Order/Item.php
@@ -0,0 +1,8 @@
+<?php
+class Ccc_Order_Model_Order_Item extends Mage_Core_Model_Abstract
+{
+    public function _construct() 
+    {
+        $this->_init('order/order_item');
+    }
+}
---
+++ app/code/local/Ccc/Order/Model/Resource/Order/Address.php
@@ -0,0 +1,8 @@
+<?php
+class Ccc_Order_Model_Resource_Order_Address extends Mage_Core_Model_Resource_Db_Abstract
+{
+    protected function _construct() 
+    {
+        $this->_init('order/order_address','address_id');
+    }
+}
